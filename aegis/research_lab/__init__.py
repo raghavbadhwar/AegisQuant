ed271a8aae1fa4111e1e8073d43fb4ef6654c97b
raghavbadhwar <raghavbadhwar7@gmail.com>
2026-08-10 00:25:16 +0530

    feat: export receipt series primitives

--- aegis/research_lab/__init__.py
+++ aegis/research_lab/__init__.py
@@ -4,6 +4,13 @@ from .boundaries import CandidateBoundaryError, validate_candidate_target
 from .experiments import ExperimentLedger
 from .outcomes import OutcomeIntegrityError, OutcomeLedger, build_postmortem
 from .promotion import authorize_promotion
+from .receipt_series import (
+    ReceiptReturnObservation,
+    ReceiptSeriesError,
+    derive_receipt_observations,
+    derive_receipt_observations_from_ledger,
+    receipt_series_hash,
+)
 from .strategy_evaluation import (
     StrategyEvaluationError,
     StrategyReturnSeries,
@@ -24,16 +31,21 @@ __all__ = [
     "ExperimentLedger",
     "OutcomeIntegrityError",
     "OutcomeLedger",
+    "ReceiptReturnObservation",
+    "ReceiptSeriesError",
     "StrategyEvaluationError",
     "StrategyReturnSeries",
     "authorize_promotion",
     "build_postmortem",
     "combinatorial_purged_splits",
     "common_sample_hash",
+    "derive_receipt_observations",
+    "derive_receipt_observations_from_ledger",
     "evaluate_predeclared_strategies",
     "interval_combinatorial_purged_splits",
     "interval_purged_walk_forward",
     "purged_walk_forward",
+    "receipt_series_hash",
     "strategy_series_hash",
     "validate_candidate_target",
     "validation_statistics",
